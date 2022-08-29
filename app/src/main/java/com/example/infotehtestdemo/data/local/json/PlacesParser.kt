0893988fa5acdf7ed7a56062c7eacd696ad386f2
kykara4uk <ivanserdechenkosergeevich@gmail.com>
2022-08-30 02:00:20 +0300

    colors-and-parser-fix

--- app/src/main/java/com/example/infotehtestdemo/data/local/json/PlacesParser.kt
+++ app/src/main/java/com/example/infotehtestdemo/data/local/json/PlacesParser.kt
@@ -5,8 +5,10 @@ import android.content.res.AssetManager
 import com.example.infotehtestdemo.data.local.json.models.PlaceItem
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.InputStreamReader
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,11 +18,22 @@ class PlacesParser @Inject constructor(private val context: Context) : JSONParse
 
     override suspend fun parse(): List<PlaceItem> {
         return withContext(Dispatchers.IO){
-            val placesString = context.assets.readAssetsFile("city_list.json")
-            val typeToken = object : TypeToken<List<PlaceItem>>() {}.type
-            Gson().fromJson(placesString, typeToken)
+            val placesStream = getInputStream("city_list.json")
+            val jsonReader = JsonReader(InputStreamReader(placesStream, "UTF-8"))
+            val placesArray = ArrayList<PlaceItem>()
+            jsonReader.beginArray()
+            val gson = Gson()
+            while (jsonReader.hasNext()){
+                val place = gson.fromJson<PlaceItem>(jsonReader, PlaceItem::class.java)
+                placesArray.add(place)
+            }
+            jsonReader.endArray()
+            jsonReader.close()
+            placesArray.toList()
         }
     }
+
+    private fun getInputStream(fileName: String) = context.assets.open(fileName)
 
     private fun AssetManager.readAssetsFile(fileName : String): String = open(fileName).bufferedReader().use{it.readText()}
 }
